c3efd4ff059efa22c3655cd8d094da6c9c7010cd
Frisan-Marcel <marcel.frisan@stud.ubbcluj.ro>
2021-11-01 16:19:31 +0200

    lab 6

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from Logic.CRUD import adaugaRezervare
 from Tests.runAllTests import runTests
 from UI.Console import runMenu
+from UI.Commands import runCommands
+from utils import getRunMethod
 
-if __name__ == "__main__":
+def main():
     runTests()
     lst = []
-    runMenu(lst)
+    runMethod = getRunMethod()
+
+    if runMethod == "menu":
+        lst = adaugaRezervare(1, "paul", "economy", 44, "da", lst)
+        lst = adaugaRezervare(2, "paul", "economy plus", 33, "nu", lst)
+        lst = adaugaRezervare(3, "paul", "business", 2, "da", lst)
+        lst = adaugaRezervare(4, "mircea", "economy", 44, "da", lst)
+
+        runMenu(lst)
+    
+    elif runMethod == "commands":
+        runCommands(lst) 
+
+if __name__ == "__main__":
+    main()
